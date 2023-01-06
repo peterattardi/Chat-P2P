1d78e0b782cb3d4e5e7301ab8ea767cf77b2405d
peteratt <pietro.attardi.pa@gmail.com>
2023-01-07 00:35:33 +0100

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Chat-P2P/',
   server: {
     port: 3000,
   },
