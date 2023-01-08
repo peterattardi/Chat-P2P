28d9047c30390cc6bf850097ef9e747591a907c5
peteratt <pietro.attardi.pa@gmail.com>
2023-01-08 18:36:07 +0100

    chore

--- src/pages/room/Room.tsx
+++ src/pages/room/Room.tsx
@@ -14,7 +14,7 @@ const Room = (): JSX.Element => {
   const { setRemoteStream, answerOffer, closeConnection, send } = useSignal({ onMessageReceived })
   const {
     state: { connectionState },
-    dispatch
+    dispatch,
   } = useContext(PeerConnectionContext)
 
   const openWebcam = async (): Promise<void> => {
@@ -24,14 +24,14 @@ const Room = (): JSX.Element => {
         payload: {
           localStream: await navigator.mediaDevices.getUserMedia({
             video: true,
-            audio: true
+            audio: true,
           }),
           onActionCompeted: ({ localStream }) => {
             if (localWebcam.current != null && localStream != null) {
               localWebcam.current.srcObject = localStream
             }
-          }
-        }
+          },
+        },
       })
 
       send('Opened webcam')
@@ -62,7 +62,7 @@ const Room = (): JSX.Element => {
     setRemoteStream(remoteWebcam.current)
   }
 
-  function onMessageReceived (message: unknown): void {
+  function onMessageReceived(message: unknown): void {
     console.log('message', message)
   }
 
@@ -73,7 +73,7 @@ const Room = (): JSX.Element => {
       case 'connecting':
         return 'Connecting...'
       case 'connected':
-        return 'Peer joined! Say hello'
+        return 'Peer joined! Open the cam 📷'
       case 'disconnected':
         return 'Peer disconnected :/'
       case 'closed':
@@ -88,28 +88,26 @@ const Room = (): JSX.Element => {
   return (
     <Layout>
       <section className='w-full min-h-screen flex flex-col justify-center items-center space-y-[32px]'>
-        {!joinedClick
-          ? (
-            <section className='space-y-[32px] flex flex-col justify-center items-center'>
-              <h1>Someone has invited you to join their room!</h1>
-              <Button onClick={start}>Join</Button>
-            </section>
-            )
-          : (
-            <>
-              <h1>{title}</h1>
-              <section className='w-full grid md:grid-cols-2 md:grid-rows-1 grid-rows-2 grid-cols-1 gap-[64px]'>
-                <div className='flex md:flex-col flex-col-reverse space-y-[12px] flex-r'>
-                  <Video ref={localWebcam} />
-                  <Button small buttonType='secondary' onClick={handleClickOpenWebcam} disabled={connectionState !== 'connected'}>
-                    Open Wecam
-                  </Button>
-                </div>
+        {!joinedClick ? (
+          <section className='space-y-[32px] flex flex-col justify-center items-center'>
+            <h1>Someone has invited you to join their room!</h1>
+            <Button onClick={start}>Join</Button>
+          </section>
+        ) : (
+          <>
+            <h1>{title}</h1>
+            <section className='w-full grid md:grid-cols-2 md:grid-rows-1 grid-rows-2 grid-cols-1 gap-[64px]'>
+              <div className='flex md:flex-col flex-col-reverse space-y-[12px] flex-r'>
+                <Video ref={localWebcam} />
+                <Button small buttonType='secondary' onClick={handleClickOpenWebcam} disabled={connectionState !== 'connected'}>
+                  Open Wecam
+                </Button>
+              </div>
 
-                <Video ref={remoteWebcam} />
-              </section>
-            </>
-            )}
+              <Video ref={remoteWebcam} />
+            </section>
+          </>
+        )}
 
         {connectionState === 'connected' && (
           <Button small onClick={hangup}>
